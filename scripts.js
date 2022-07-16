4537afc7861459d89708985dfdc94602d7a98c53
doozles411 <cindy.phelps01@gmail.com>
2022-07-15 19:15:34 -0700

    Clean up code

--- scripts.js
+++ scripts.js
@@ -34,6 +34,7 @@ function playRound(playerSelection, computerSelection) {
 function game() {
     let playerScore = 0;
     let computerScore = 0;
+    
     for (let i = 0; i < 5; i++) {
         let answer = prompt("Enter a choice: ");
         let result = playRound(answer);
@@ -41,24 +42,25 @@ function game() {
         if (result === "It's a tie!") {
             playerScore++;
             computerScore++;
-            console.log(`Your score: ${playerScore}`);
-            console.log(`Computer score: ${computerScore}`);
         } else if (result === "Cockadoodledoo! You win!") {
             playerScore++;
-            console.log(`Your score: ${playerScore}`);
-            console.log(`Computer score: ${computerScore}`);
         } else if (result === "You lose... bok BOK!") {
             computerScore++;
-            console.log(`Your score: ${playerScore}`);
-            console.log(`Computer score: ${computerScore}`);
         }
     }
+
     if (playerScore === computerScore) {
         console.log("It's a tie! No one wins.");
+        console.log(`Your score: ${playerScore}`);
+        console.log(`Computer score: ${computerScore}`);
     } else if (playerScore > computerScore) {
         console.log("You win!");
+        console.log(`Your score: ${playerScore}`);
+        console.log(`Computer score: ${computerScore}`);
     } else {
         console.log("You lose!");
+        console.log(`Your score: ${playerScore}`);
+        console.log(`Computer score: ${computerScore}`);
     }
 }
 
