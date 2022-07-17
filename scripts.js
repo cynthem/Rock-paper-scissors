39b1474dcf5a68c0a85aa0922e3f6ae84e0852f8
doozles411 <cindy.phelps01@gmail.com>
2022-07-17 16:16:07 -0700

    Add dom captures of results

--- scripts.js
+++ scripts.js
@@ -5,13 +5,13 @@ let computerScore = 0;
 const featherSelection = document.getElementById('feather');
 const eggSelection = document.getElementById('egg');
 const beakSelection = document.getElementById('beak');
-
-// Functions are created within event listeners to reference for later event listener removal
-function gamePlay() {
-    featherSelection.addEventListener('click', featherPlayed = () => playRound('Feather', getComputerChoice));
-    eggSelection.addEventListener('click', eggPlayed = () => playRound('Egg', getComputerChoice));
-    beakSelection.addEventListener('click', beakPlayed = () => playRound('Beak', getComputerChoice));
-}
+// DOM captures of results
+const playerScored = document.getElementById('.player');
+const computerScored = document.getElementById('.computer');
+const playerImage = document.getElementById('.player-result');
+const computerImage = document.getElementById('.computer-result');
+const resultTextTop = document.getElementById('.result-text-1');
+const resultTextBottom = document.getElementById('.result-text-2');
 
 function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * 3);
@@ -25,26 +25,36 @@ function getComputerChoice() {
     }
 }
 
+// Functions are created within event listeners to reference for later event listener removal
+function gamePlay() {
+    featherSelection.addEventListener('click', featherPlayed = () => playRound('Feather', getComputerChoice));
+    eggSelection.addEventListener('click', eggPlayed = () => playRound('Egg', getComputerChoice));
+    beakSelection.addEventListener('click', beakPlayed = () => playRound('Beak', getComputerChoice));
+}
+
 function playRound(playerSelection, computerSelection) {
     computerSelection = getComputerChoice();
     if (playerSelection === computerSelection) {
-        console.log("Tie")
         return "It's a tie!";
     } else if (
         (playerSelection === 'Feather' && computerSelection === 'Beak') || 
         (playerSelection === 'Beak' && computerSelection === 'Egg') ||
         (playerSelection === 'Egg' && computerSelection === 'Feather') 
     ) {
-        console.log("Win")
         return "Cockadoodledoo! You win!";
     } else if (
         (playerSelection === 'Feather' && computerSelection === 'Egg') || 
         (playerSelection === 'Beak' && computerSelection === 'Feather') ||
         (playerSelection === 'Egg' && computerSelection === 'Beak') 
     ) {
-        console.log("Lose")
         return "You lose... bok BOK!";
     }
+}
+
+function draw(player, computer) {
+
+
+
 }
 
 gamePlay();
